7ee67b3a83a742ce2c45a0a16095fd135b39e64f
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-06-09 23:07:26 -0300

    refactor: renomeia váriaveis no arquivo carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,8 @@
 const track = document.querySelector('.carousel-track');
 const slides = document.querySelectorAll('.carousel-slide');
 const dotsCarousel = document.getElementById('carousel-dots');
-let currentIndex = 0;
-let dots = [];
+let carouselCurrentIndex = 0;
+let carouselDots = [];
 
 function createDots() {
     for (let i = 0; i < slides.length; i++) {
@@ -10,31 +10,31 @@ function createDots() {
         dot.classList.add('dot');
         if (i === 0) dot.classList.add('active');
         dot.addEventListener('click', () => {
-            currentIndex = i;
-            updateCarousel(currentIndex);
+            carouselCurrentIndex = i;
+            updateCarousel(carouselCurrentIndex);
         });
         dotsCarousel.appendChild(dot);
-        dots.push(dot);
+        carouselDots.push(dot);
     }
 }
 
 function updateCarousel(index) {
     track.style.transform = `translateX(-${index * 100}%)`;
 
-    dots.forEach(dot => dot.classList.remove('active'));
-    if (dots[index]) {
-        dots[index].classList.add('active');
+    carouselDots.forEach(dot => dot.classList.remove('active'));
+    if (carouselDots[index]) {
+        carouselDots[index].classList.add('active');
     }
 }
 
 document.querySelector('.carousel-btn.next').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slides.length;
-    updateCarousel(currentIndex);
+    carouselCurrentIndex = (carouselCurrentIndex + 1) % slides.length;
+    updateCarousel(carouselCurrentIndex);
 });
 
 document.querySelector('.carousel-btn.prev').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    updateCarousel(currentIndex);
+    carouselCurrentIndex = (carouselCurrentIndex - 1 + slides.length) % slides.length;
+    updateCarousel(carouselCurrentIndex);
 });
 
 // Inicialização
